ed04db5dddc0356286d21ad95cd5fd8c913b1e74
Fernando Gutierrez <fernandogutierrezjr0@gmail.com>
2022-08-17 11:32:10 -0500

    Created pAequorFactory function(). This function goal is to create objects. So far this object has 3 value pairs, specimenNum, dna and mutate. specimenNum is int value, dna returns a array[15] DNA strand, mutate returns the DNA array with one of the strands changed.

--- main.js
+++ main.js
@@ -13,7 +13,53 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//function used to create multiple specimen objects.
+//The first parameter is a number (no two organisms should have the same number).
+//The second parameter is an array of 15 DNA bases.
+const pAequorFactory = (specNum, specDna) => {
+  //object specObject
+  const specObject = {
+    'specimenNum': specNum,
+    'dna': specDna = mockUpStrand(),
+    'mutate': []
+  };
 
+  if (specObject.mutate)
+  {
+    //copying specObject.dna array into specObject.mutate
+    for (let i = 0; i < specObject.dna.length; i++)
+    {
+      specObject.mutate.push(specObject.dna[i])
+    }
+    //calling returnRandBase function to return random DNA base ['A', 'T', 'C', 'G']
+    let mutateDNA = returnRandBase();
+    // assigning variable to a random number between 0 - 15
+    let randomNum = Math.floor(Math.random() * 15)
+    //console.log(`mutated DNA ${mutateDNA}`)
+    //console.log(`DNA that corresponds with random number ${randomNum} is ${specObject.mutate[randomNum]}`);
+    specObject.mutate[randomNum] = mutateDNA;
+    //checking if the random mutateDNA is the same as the DNA in the object.dna array.
+    if(specObject.mutate[randomNum] === specObject.dna[randomNum])
+    {
+      //calling returnRandBase again to get a different DNA.
+      mutateDNA = returnRandBase();
+      //console.log(`mutated DNA ${mutateDNA}`)
+      specObject.mutate[randomNum] = mutateDNA;
+    }
+    //console.log(specObject.mutate)
+  }
+
+  return specObject;
+};
+
+
+//testing functions
+let spec1;
+let test = pAequorFactory(1, spec1);
+
+console.log(`Specimen Number: ${test.specimenNum}`);
+console.log(`Specimen DNA: ${test.dna.join('')}`);
+console.log(`Mutated DNA: ${test.mutate.join('')}`);
 
 
 
