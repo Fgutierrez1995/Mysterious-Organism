4938eb3778ccca93d00b05f9cfa654a3d62a761c
Fernando Gutierrez <fernandogutierrezjr0@gmail.com>
2022-08-18 09:15:05 -0500

    Implemented compareDNA function to the ppAequorFactory function. This function compares 2 different specimens dna and returns the % of common DNA.

--- main.js
+++ main.js
@@ -21,7 +21,25 @@ const pAequorFactory = (specNum, specDna) => {
   const specObject = {
     'specimenNum': specNum,
     'dna': specDna = mockUpStrand(),
-    'mutate': []
+    'mutate': [],
+    //adding function that compares 2 different specimens DNA
+    //and logs the percentage of common DNA.
+    //function takes another obj paremeter
+    compareDNA(pAequor) {
+      //creating empty array for common DNA.
+      let commonDna = [];
+      for (let i = 0; i < specObject.dna.length; i++)
+      {
+        //if both specimen DNA is the same, then add to the commonDNA array.
+        if (pAequor.dna[i] === specObject.dna[i])
+        {
+          commonDna.push(specObject.dna[i]);
+        }
+      }
+      //calculating the percentage of common DNA.
+      let numOfDNA = (commonDna.length / 15) * 100;
+      console.log(`Specimen #1 and Specimen #2 have ${numOfDNA.toFixed(0)}% DNA in common.`)
+    }
   };
 
   if (specObject.mutate)
@@ -35,34 +53,30 @@ const pAequorFactory = (specNum, specDna) => {
     let mutateDNA = returnRandBase();
     // assigning variable to a random number between 0 - 15
     let randomNum = Math.floor(Math.random() * 15)
-    //console.log(`mutated DNA ${mutateDNA}`)
-    //console.log(`DNA that corresponds with random number ${randomNum} is ${specObject.mutate[randomNum]}`);
     specObject.mutate[randomNum] = mutateDNA;
     //checking if the random mutateDNA is the same as the DNA in the object.dna array.
     if(specObject.mutate[randomNum] === specObject.dna[randomNum])
     {
       //calling returnRandBase again to get a different DNA.
       mutateDNA = returnRandBase();
-      //console.log(`mutated DNA ${mutateDNA}`)
       specObject.mutate[randomNum] = mutateDNA;
     }
-    //console.log(specObject.mutate)
   }
 
-  return specObject;
+  return specObject; 
+
 };
 
 
 //testing functions
 let spec1;
-let test = pAequorFactory(1, spec1);
+let spec2;
+let test1 = pAequorFactory(1, spec1);
+let test2 = pAequorFactory(2, spec2);
 
-console.log(`Specimen Number: ${test.specimenNum}`);
-console.log(`Specimen DNA: ${test.dna.join('')}`);
-console.log(`Mutated DNA: ${test.mutate.join('')}`);
-
-
-
-
-
-
+console.log(`Specimen Number: ${test1.specimenNum}`);
+console.log(`Specimen #1 DNA: ${test1.dna.join('')}`);
+console.log(`Mutated DNA: ${test1.mutate.join('')}`); 
+console.log(`Specimen Number: ${test2.specimenNum}`);
+console.log(`Specimen #2 DNA: ${test2.dna.join('')}`);
+test1.compareDNA(test2);
